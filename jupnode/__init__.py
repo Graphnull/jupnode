922ccc24d6f637bc1ab51c92b5e818bb42bd5de0
vasyainformatica <01vasil@mail.ru>
2021-05-28 12:39:14 +0300

    test in google colab

--- jupnode/__init__.py
+++ jupnode/__init__.py
@@ -1,4 +1,5 @@
 from IPython.core.magic import register_cell_magic
+from IPython.core.inputtransformer import StatelessInputTransformer
 from IPython.display import display, HTML
 from IPython.core.error import TryNext
 import warnings
@@ -16,8 +17,20 @@ def shutdown_hook(ipython):
     nodeapp.terminate()
     raise TryNext
 
+ip = get_ipython()
 
-def modify_for_node(lines):
+@StatelessInputTransformer.wrap
+def modify_for_node(cell):
+    lines = cell.partition('\n')
+    if lines[0].strip() == '%%py':
+        lines.pop(0)
+    else:
+        lines.insert(0, '(async()=>{\n')
+        lines.insert(0, '%%node\n')
+        lines.append('})()')
+    return "\n".join(lines)
+
+def modify_for_node_for_new_version(lines):
     if lines[0].strip() == '%%py':
         lines.pop(0)
     else:
@@ -26,5 +39,8 @@ def modify_for_node(lines):
         lines.append('})()')
     return lines
 
-ip = get_ipython()
-ip.input_transformers_cleanup.append(modify_for_node)
+if hasattr(ip, 'input_transformers_cleanup'):
+    ip.input_transformers_cleanup.append(modify_for_node_for_new_version)
+else:
+    ip.input_transformer_manager.logical_line_transforms.append(modify_for_node())
+
