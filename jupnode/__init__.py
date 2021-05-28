2cec5bf92e794aa595cca03bfefc5bc60bf77bb5
vasyainformatica <01vasil@mail.ru>
2021-05-28 13:33:17 +0300

    test for google colab

--- jupnode/__init__.py
+++ jupnode/__init__.py
@@ -19,16 +19,6 @@ def shutdown_hook(ipython):
 
 ip = get_ipython()
 
-@StatelessInputTransformer.wrap
-def modify_for_node(cell):
-    lines = cell.partition('\n')
-    if lines[0].strip() == '%%py':
-        lines.pop(0)
-    else:
-        lines.insert(0, '(async()=>{\n')
-        lines.insert(0, '%%node\n')
-        lines.append('})()')
-    return "\n".join(lines)
 
 def modify_for_node_for_new_version(lines):
     if lines[0].strip() == '%%py':
@@ -39,8 +29,30 @@ def modify_for_node_for_new_version(lines):
         lines.append('})()')
     return lines
 
+class NodeInputTransformer():
+    def __init__(self):
+        self.lineNumber=0
+        print('inited')
+    
+    def wrap(self, line):
+      print(line)
+      return line
+    def push(self, line):
+        if self.lineNumber==0:
+          return '%%node\n(async (){\n'+line
+        return line
+    
+    def reset(self):
+        if self.lineNumber!=0:
+          self.lineNumber=0
+          return '})()'
+        self.lineNumber=0
+        print('reset')
+        """No-op - exists for compatibility."""
+        pass
+
 if hasattr(ip, 'input_transformers_cleanup'):
     ip.input_transformers_cleanup.append(modify_for_node_for_new_version)
 else:
-    ip.input_transformer_manager.logical_line_transforms.append(modify_for_node())
+    ip.input_transformer_manager.logical_line_transforms.append(NodeInputTransformer())
 
