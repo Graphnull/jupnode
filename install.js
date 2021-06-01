746c154627ef39c184551d9bdfc9dc89ef2371a4
vasyainformatica <01vasil@mail.ru>
2021-06-01 15:32:36 +0300

    test in colab

--- install.js
+++ install.js
@@ -1,14 +1,14 @@
 let child_process= require('child_process')
-// let list = child_process.execSync(`pip freeze`).toString().split('\n').filter(v=>{
-//     let parseLine = v.split('=')
-//     return parseLine[0]==='jupnode'||parseLine[parseLine.length-1]==='jupnode'
-// })
+let list = child_process.execSync(`pip freeze`).toString().split('\n').filter(v=>{
+    let parseLine = v.split('=')
+    return parseLine[0]==='jupnode'||parseLine[parseLine.length-1]==='jupnode'
+})
 
-// let result = child_process.execSync(`pip install --upgrade --force-reinstall -e `+__dirname)
+let result = child_process.execSync(`pip install jupnode --no-index --find-links file://`+__dirname)
 
 //for debug
 // console.log(list);
-// console.log(result.toString())
+console.log(result.toString())
 
 
-console.log(child_process.execSync(`pip install git+https://github.com/graphnull/jupnode`).toString())
+// console.log(child_process.execSync(`pip install git+https://github.com/graphnull/jupnode`).toString())
