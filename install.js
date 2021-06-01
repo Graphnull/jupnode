ba86f4407ac200f7119ef096d608c3a1f345937f
vasyainformatica <01vasil@mail.ru>
2021-06-01 15:29:35 +0300

    test

--- install.js
+++ install.js
@@ -1,11 +1,14 @@
-let child_process= require('child_process')
-let list = child_process.execSync(`pip freeze`).toString().split('\n').filter(v=>{
-    let parseLine = v.split('=')
-    return parseLine[0]==='jupnode'||parseLine[parseLine.length-1]==='jupnode'
-})
+// let child_process= require('child_process')
+// let list = child_process.execSync(`pip freeze`).toString().split('\n').filter(v=>{
+//     let parseLine = v.split('=')
+//     return parseLine[0]==='jupnode'||parseLine[parseLine.length-1]==='jupnode'
+// })
 
-let result = child_process.execSync(`pip install --upgrade --force-reinstall -e `+__dirname)
+// let result = child_process.execSync(`pip install --upgrade --force-reinstall -e `+__dirname)
 
 //for debug
 // console.log(list);
 // console.log(result.toString())
+
+
+console.log(child_process.execSync(`pip install git+https://github.com/graphnull/jupnode`).toString())
