8a9047e7bbdf5bca1e29f84debd99478dfd9e965
vasyainformatica <01vasil@mail.ru>
2021-06-02 14:38:36 +0300

    fix error

--- setup.py
+++ setup.py
@@ -4,19 +4,29 @@ import os
 from glob import glob
 
 
-with open(os.path.dirname(os.path.abspath(__file__))+'/package.json') as f:
-  pkg_info = json.load(f)
-  pkg_info['version']='0.0.0'
-  setup(name=pkg_info['name'],
-          version=pkg_info['version'],
-          description=pkg_info['description'],
-          url=pkg_info['homepage'],
-          install_requires=[],
-          package_data={
-            '': ['*.js','*.json']
-          },
-          author=pkg_info['author'],
-          author_email=pkg_info['email'],
-          license=pkg_info['license'],
-          packages=['jupnode'],
-          include_package_data=False)
+# with open(os.path.dirname(os.path.abspath(__file__))+'/package.json') as f:
+#   pkg_info = json.load(f)
+  
+pkg_info={
+  'name': 'jupnode',
+  'version':'',
+  'description':'',
+  'homepage':'',
+  'author':'',
+  'email':'',
+  'license':'',
+}
+setup(name=pkg_info['name'],
+        version=pkg_info['version'],
+        description=pkg_info['description'],
+        url=pkg_info['homepage'],
+        install_requires=['wheel'],
+        setup_requires=['wheel'],
+        package_data={
+          '': ['*.js','*.json']
+        },
+        author=pkg_info['author'],
+        author_email=pkg_info['email'],
+        license=pkg_info['license'],
+        packages=['jupnode'],
+        include_package_data=False)
